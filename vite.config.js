7ab87c6577d611039873f55eed2542ad64c7c8e2
parkashkumarcs <parkashkumar.bscs.official@gmail.com>
2025-01-30 16:49:30 +0500

    update the code

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/", // Set base to the GitHub repo name
-  css: {
-    modules: {
+  css:{
+    modules:{
       localsConvention: "camelCase",
     },
   },
-});
+})
