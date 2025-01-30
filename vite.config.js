4fee60356a55a02f64845c15e4c45546ada1ffa9
parkashkumarcs <parkashkumar.bscs.official@gmail.com>
2025-01-30 16:37:49 +0500

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/myPortfolioPK/", // Set base to the GitHub repo name
+  base: "/portfolio/", // Set base to the GitHub repo name
   css: {
     modules: {
       localsConvention: "camelCase",
